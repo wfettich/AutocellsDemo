f905c7808d66632cd2a9eeb3aa75a515a617ca99
Walter Fettich <wfettich@gmail.com>
2019-04-04 12:39:44 +0300

    add label

--- Demo/ImageAndTextView.swift
+++ Demo/ImageAndTextView.swift
@@ -12,6 +12,7 @@ class ImageAndTextView: UIView
 {
     @IBOutlet weak var viewImage: UIImageView!
     @IBOutlet weak var viewText: UITextView!
+    @IBOutlet weak var labelText: UILabel!
     
     var modelObject:ModelObject?
     {
@@ -19,12 +20,28 @@ class ImageAndTextView: UIView
         {
             guard modelObject != nil else {return}
             
+            viewImage.isHidden = true
             //viewImage.image = modelObject?.urlImage //to do: get image from url
-            viewText.text = modelObject!.text
-            
+            viewText.text = "text view: " + (modelObject!.text ?? "")  + " END text view"
+            labelText.text = "label: " + (modelObject!.text ?? "") + " END label"
+
             setNeedsUpdateConstraints()
+            setNeedsLayout()
         }
     }
+    
+//    override func layoutSubviews()
+//    {
+//        super.layoutSubviews()
+//
+//        if self.viewText.bounds.width != self.bounds.width
+//        {
+//            self.viewText.preferredMaxLayoutWidth = self.bounds.width;
+//            viewText.sizeToFit()
+//
+//            heightConstraintAboutText.constant = labelAbout.bounds.size.height + 50
+//        }
+//    }
     
     override func awakeAfter(using aDecoder: NSCoder) -> Any? {
         guard subviews.isEmpty else {
